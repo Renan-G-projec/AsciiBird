67419ed7d82dfaf3b15c276b6673317b084d6dd3
Renan-G-projec <renanfartes@gmail.com>
2026-06-29 11:57:59 -0300

    add: InputHandler interface added. AMDG

---
+++ include/inputHandler.hpp
@@ -0,0 +1,19 @@
+// Ad Maiorem Dei Gloriam!
+#ifndef INPUT_HANDLER_HPP
+#define INPUT_HANDLER_HPP
+
+class InputHandler {
+public:
+
+    static void init();
+    static void finish();
+
+    static void update();
+
+    static bool isKeyPressed(const char);
+
+private:
+    bool mKeyPressedTable[256] = {0};
+};
+
+#endif
